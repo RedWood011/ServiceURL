cf2ab70a7e1e74f557ed9de26532eb1fec266f81
RedWood011 <46817256+RedWood011@users.noreply.github.com>
2023-06-10 16:57:42 +0300

    Iter22 (#13)
    * iter19
    
    * fix test 17
    
    * fix
    
    * add test package main  osexitcheck_test.go
    
    * fix
    
    * fix
    
    * iter 22
    
    * iter 22
    
    * Revert "iter 22"
    
    This reverts commit 97d943163d4cec083e2653c6f1b0a6b7ad9e73d9.
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    ---------
    
    Co-authored-by: Evgen Yaroshenko <yaev96@list.ru>

---
+++ cmd/generate_ssl_key/main.go
@@ -0,0 +1,95 @@
+package main
+
+import (
+	"bytes"
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/x509"
+	"crypto/x509/pkix"
+	"encoding/pem"
+	"log"
+	"math/big"
+	"net"
+	"os"
+	"time"
+)
+
+func generate() {
+	cert := &x509.Certificate{
+		// указываем уникальный номер сертификата
+		SerialNumber: big.NewInt(1658),
+		// заполняем базовую информацию о владельце сертификата
+		Subject: pkix.Name{
+			Organization: []string{"Evgeniy Yaroshenko"},
+			Country:      []string{"RU"},
+			Locality:     []string{"Moscow"},
+		},
+		// разрешаем использование сертификата для 127.0.0.1 и ::1
+		IPAddresses: []net.IP{net.IPv4(127, 0, 0, 1), net.IPv6loopback},
+		// сертификат верен, начиная со времени создания
+		NotBefore: time.Now(),
+		// время жизни сертификата — 1 год
+		NotAfter:     time.Now().AddDate(1, 0, 0),
+		SubjectKeyId: []byte{0, 8, 9, 4, 7, 6, 6, 3, 4, 1, 4},
+		// устанавливаем использование ключа для цифровой подписи, а также клиентской и серверной авторизации
+		ExtKeyUsage: []x509.ExtKeyUsage{x509.ExtKeyUsageClientAuth, x509.ExtKeyUsageServerAuth},
+		KeyUsage:    x509.KeyUsageDigitalSignature,
+	}
+
+	// создаём новый приватный RSA-ключ длиной 4096 бит
+
+	privateKey, err := rsa.GenerateKey(rand.Reader, 4096)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// создаём x.509-сертификат
+	certBytes, err := x509.CreateCertificate(rand.Reader, cert, cert, &privateKey.PublicKey, privateKey)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// кодируем сертификат и ключ в формате PEM, который используется для хранения и обмена криптографическими ключами
+	var certPEM bytes.Buffer
+	err = pem.Encode(&certPEM, &pem.Block{
+		Type:  "CERTIFICATE",
+		Bytes: certBytes,
+	})
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fileserver, err := os.Create("server_crt.crt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	_, err = fileserver.Write(certPEM.Bytes())
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var privateKeyPEM bytes.Buffer
+	err = pem.Encode(&privateKeyPEM, &pem.Block{
+		Type:  "RSA PRIVATE KEY",
+		Bytes: x509.MarshalPKCS1PrivateKey(privateKey),
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	filekey, err := os.Create("server_key.key")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	_, err = filekey.Write(privateKeyPEM.Bytes())
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func main() {
+	generate()
+}
--- internal/config/cfg.go
+++ internal/config/cfg.go
@@ -2,54 +2,177 @@
 package config
 
 import (
+	"encoding/json"
 	"flag"
+	"io"
+	"log"
 	"os"
+
+	"github.com/caarlos0/env"
+)
+
+const (
+	serverAddress   = ":8080"
+	baseURL         = "http://localhost:8080"
+	fileStoragePath = ""
+	keyHash         = "7cdb395a-e63e-445f-b2c4-90a400438ee4"
+	//databaseDSN       = "postgres://qwerty:qwerty@localhost:5438/postgres?sslmode=disable"
+	databaseDSN      = ""
+	countRepetitionB = 5
+	amountWorkers    = 5
+	sizeBufWorker    = 100
+	isHTTPS          = false
 )
 
 // Config Конфигурация приложения
 type Config struct {
-	Port              string
-	Address           string
-	FilePath          string
-	KeyHash           string
-	DatabaseDSN       string
-	CountRepetitionBD string
-	NumWorkers        int
-	SizeBufWorker     int
+	ServerAddress     string `env:"SERVER_ADDRESS" envDefault:""`
+	BaseURL           string `env:"BASE_URL" envDefault:""`
+	FilePath          string `env:"FILE_STORAGE_PATH" envDefault:""`
+	KeyHash           string `env:"KEY_HASH" envDefault:""`
+	DatabaseDSN       string `env:"DATABASE_DSN" envDefault:""`
+	CountRepetitionBD int    `env:"REPETITION_CONNECT" envDefault:""`
+	IsHTTPS           bool   `env:"ENABLE_HTTPS" envDefault:""`
+	AmountWorkers     int    `env:"AMOUNT_WORKERS" envDefault:""`
+	SizeBufWorker     int    `env:"BUF_WORKERS" envDefault:""`
+	ConfigPath        string
+}
+
+// JSONConfig Конфигурация приложения json-файлом
+type JSONConfig struct {
+	BaseURL         string `json:"base_url"`
+	ServerAddress   string `json:"server_address"`
+	FileStoragePath string `json:"file_storage_path"`
+	DatabaseDSN     string `json:"database_dsn"`
+	IsHTTPS         bool   `json:"enable_https"`
 }
 
 // NewConfig Создание конфигурации
 func NewConfig() *Config {
+
 	cfg := &Config{}
-	if cfg.Port = os.Getenv("SERVER_ADDRESS"); cfg.Port == "" {
-		flag.StringVar(&cfg.Port, "a", ":8080", "Server port")
+	cfg.parseEnv()
+	cfg.parseFlags()
+
+	fileCfg := Config{}
+	switch {
+	case cfg.ConfigPath != "":
+		fileCfg = readConfigFromFIle(cfg.ConfigPath)
+	default:
+		fileCfg.ServerAddress = serverAddress
+		fileCfg.BaseURL = baseURL
+		fileCfg.FilePath = fileStoragePath
+		fileCfg.DatabaseDSN = databaseDSN
+		fileCfg.CountRepetitionBD = countRepetitionB
+		fileCfg.AmountWorkers = amountWorkers
+		fileCfg.SizeBufWorker = sizeBufWorker
+		fileCfg.IsHTTPS = isHTTPS
+		fileCfg.KeyHash = keyHash
 	}
 
-	if cfg.Address = os.Getenv("BASE_URL"); cfg.Address == "" {
-		flag.StringVar(&cfg.Address, "b", "http://localhost:8080", "Server  URL")
+	if cfg.ServerAddress == serverAddress {
+		cfg.ServerAddress = fileCfg.ServerAddress
 	}
-
-	if cfg.FilePath = os.Getenv("FILE_STORAGE_PATH"); cfg.FilePath == "" {
-		//flag.StringVar(&cfg.FilePath, "f", "/Users/evyaroshen/GolandProjects/yandex/ServiceURL/words.json", "File storage path")
-		flag.StringVar(&cfg.FilePath, "f", "", "File storage path")
+	if cfg.BaseURL == baseURL {
+		cfg.BaseURL = fileCfg.BaseURL
 	}
-
-	if cfg.KeyHash = os.Getenv("KEY_HASH"); cfg.KeyHash == "" {
-		flag.StringVar(&cfg.KeyHash, "key", "7cdb395a-e63e-445f-b2c4-90a400438ee4", "KeyHash secret")
+	if cfg.FilePath == fileStoragePath {
+		cfg.FilePath = fileCfg.FilePath
 	}
-
-	if cfg.DatabaseDSN = os.Getenv("DATABASE_DSN"); cfg.DatabaseDSN == "" {
-		flag.StringVar(&cfg.DatabaseDSN, "d", "", "Database connection")
-		//flag.StringVar(&cfg.DatabaseDSN, "d", "postgres://qwerty:qwerty@localhost:5438/postgres?sslmode=disable", "")
+	if cfg.DatabaseDSN == databaseDSN {
+		cfg.DatabaseDSN = fileCfg.DatabaseDSN
 	}
-
-	if cfg.CountRepetitionBD = os.Getenv("REPETITION_CONNECT"); cfg.CountRepetitionBD == "" {
-		flag.StringVar(&cfg.CountRepetitionBD, "repetition", "5", "repetition connect database")
+	if cfg.CountRepetitionBD == countRepetitionB {
+		cfg.CountRepetitionBD = fileCfg.CountRepetitionBD
 	}
-	cfg.NumWorkers = 5
-	cfg.SizeBufWorker = 100
-
-	flag.Parse()
+	if cfg.AmountWorkers == amountWorkers {
+		cfg.AmountWorkers = fileCfg.AmountWorkers
+	}
+	if cfg.SizeBufWorker == sizeBufWorker {
+		cfg.SizeBufWorker = fileCfg.SizeBufWorker
+	}
+	if !cfg.IsHTTPS {
+		cfg.IsHTTPS = fileCfg.IsHTTPS
+	}
+	if cfg.KeyHash == keyHash {
+		cfg.KeyHash = fileCfg.KeyHash
+	}
 
 	return cfg
+}
+
+func (c *Config) parseEnv() {
+	err := env.Parse(c)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func (c *Config) parseFlags() {
+	if isDefault(c.ServerAddress) {
+		flag.StringVar(&c.ServerAddress, "a", serverAddress, "Server port")
+	}
+	if isDefault(c.BaseURL) {
+		flag.StringVar(&c.BaseURL, "b", baseURL, "Server  URL")
+	}
+	if isDefault(c.FilePath) {
+		flag.StringVar(&c.FilePath, "f", fileStoragePath, "File storage path")
+	}
+	if isDefault(c.KeyHash) {
+		flag.StringVar(&c.KeyHash, "key", keyHash, "KeyHash secret")
+	}
+	if isDefault(c.DatabaseDSN) {
+		flag.StringVar(&c.DatabaseDSN, "d", databaseDSN, "Database DSN")
+	}
+	if isDefault(c.CountRepetitionBD) {
+		flag.IntVar(&c.CountRepetitionBD, "r", countRepetitionB, "CountRepetitionBD")
+	}
+
+	if isDefault(c.AmountWorkers) {
+		flag.IntVar(&c.AmountWorkers, "workers", amountWorkers, "Number of workers")
+	}
+	if isDefault(c.SizeBufWorker) {
+		flag.IntVar(&c.SizeBufWorker, "buff", sizeBufWorker, "Workers channel buffer")
+	}
+
+	if isDefault(c.IsHTTPS) {
+		flag.BoolVar(&c.IsHTTPS, "https", isHTTPS, "Enable HTTPS")
+	}
+
+	flag.StringVar(&c.ConfigPath, "config", "", "configuration file")
+
+	flag.Parse()
+}
+
+func isDefault[T comparable](v T) bool {
+	var zero T
+	return v == zero
+}
+
+func readConfigFromFIle(fileName string) Config {
+	f, err := os.Open(fileName)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	data, err := io.ReadAll(f)
+	if err != nil {
+		log.Fatal()
+	}
+	cfg := JSONConfig{}
+	err = json.Unmarshal(data, &cfg)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return Config{
+		ServerAddress:     cfg.ServerAddress,
+		BaseURL:           cfg.BaseURL,
+		FilePath:          cfg.FileStoragePath,
+		DatabaseDSN:       cfg.DatabaseDSN,
+		IsHTTPS:           cfg.IsHTTPS,
+		CountRepetitionBD: countRepetitionB,
+		AmountWorkers:     amountWorkers,
+		SizeBufWorker:     sizeBufWorker,
+	}
+
 }
